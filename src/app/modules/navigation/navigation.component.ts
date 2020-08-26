8930ca6ac52c407bd852b01e1999ed6ad0df4965
hppod <hppodgornik@gmail.com>
2020-08-25 22:14:18 -0300

    Implementação do componente welcome

--- src/app/modules/navigation/navigation.component.ts
+++ src/app/modules/navigation/navigation.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  title: string = 'Meus filmes favoritos'
+  title: string = 'Filmoteca'
 
   constructor() { }
 
