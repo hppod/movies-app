38dee83369c029e73df7cad95904dc523773152a
hppod <hppodgornik@gmail.com>
2020-10-03 14:47:15 -0300

    Implementando a visualização de um diretor

--- src/app/modules/directors/directors-routing.module.ts
+++ src/app/modules/directors/directors-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DirectorDetailComponent } from './director-detail/director-detail.component';
 
 import { DirectorsComponent } from "./directors.component"
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: DirectorsComponent
+  },
+  {
+    path: 'detail/:directorName',
+    component: DirectorDetailComponent
   }
 ];
 
--- src/app/modules/directors/directors.module.ts
+++ src/app/modules/directors/directors.module.ts
@@ -7,17 +7,19 @@ import { FlexLayoutModule } from "@angular/flex-layout"
 import { DirectorsRoutingModule } from './directors-routing.module';
 import { DirectorsComponent } from './directors.component';
 import { ComponentsModule } from "./../../components/components.module";
-import { DirectorCardComponent } from './director-card/director-card.component'
-
+import { DirectorCardComponent } from './director-card/director-card.component';
+import { DirectorDetailComponent } from './director-detail/director-detail.component'
+import { MoviesModule } from "./../movies/movies.module"
 
 @NgModule({
-  declarations: [DirectorsComponent, DirectorCardComponent],
+  declarations: [DirectorsComponent, DirectorCardComponent, DirectorDetailComponent],
   imports: [
     CommonModule,
     DirectorsRoutingModule,
     MatCardModule,
     FlexLayoutModule,
-    ComponentsModule
+    ComponentsModule,
+    MoviesModule
   ]
 })
 export class DirectorsModule { }
